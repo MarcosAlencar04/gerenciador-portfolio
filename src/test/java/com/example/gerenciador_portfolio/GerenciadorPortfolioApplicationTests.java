02584a5089001942f443c585795d2fa6e215ef02
marcos alencar <marcos.12.01.07@gmail.com>
2025-04-22 16:45:17 -0300

    first commit

---
+++ src/test/java/com/example/gerenciador_portfolio/GerenciadorPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gerenciador_portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
